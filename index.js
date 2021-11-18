de763f4696cf65e774aeb72dbcf7e07963adfe4d
karpova.m <34@3389.ru>
2021-11-18 13:14:36 +0700

    1. Базовые типы

---
+++ index.js
@@ -0,0 +1,22 @@
+//1. Базовые типы
+//строка
+var firstName = 'Marina';
+var lastName = 'Фамилия';
+//число
+var num = 23;
+// let sum: number = num + '1'
+var sum = num + 1;
+//массив
+var arNumberAny = ['1', 2, 3];
+var arNumber = [1, 2, 3];
+var arNumberArray = [1, 2, 3];
+var arNumberArrayAny = [1, '2', 3];
+var arString = ['H', 'e', 'l'];
+var arStringArray = ['H', 'e', 'l'];
+//булево
+var isActive = !!true;
+var active;
+if (isActive === true) {
+    active = 'Активно';
+}
+active = 'Не активно';
