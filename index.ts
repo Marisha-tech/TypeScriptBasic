f0107c71ae8379a7a55b88f698e6f8f1aedbd031
karpova.m <34@3389.ru>
2021-11-18 11:12:59 +0700

    базовые типы

---
+++ index.ts
@@ -0,0 +1,29 @@
+//1. Базовые типы
+
+//строка
+const firstName: string = 'Marina'
+let lastName: string = 'Фамилия'
+
+//число
+const num: number = 23
+// let sum: number = num + '1'
+let sum: number = num + 1
+
+//массив
+let arNumberAny: any = ['1', 2, 3]
+let arNumber: number[] = [1, 2, 3]
+let arNumberArray: Array<number> = [1, 2, 3]
+let arNumberArrayAny: Array<any> = [1, '2', 3]
+let arString: string[] = ['H', 'e', 'l']
+let arStringArray: Array<string> = ['H', 'e', 'l']
+
+//булево
+let isActive: boolean = !!true
+let active
+if (isActive === true) {
+    active = 'Активно'
+}
+active = 'Не активно'
+
+
+
